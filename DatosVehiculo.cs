c1d02ca9d5055d7fbc8dff056a58545b9bb22aab
Julian Xiloj <juel145@mesoamericana.edu.gt>
2021-03-07 10:37:17 -0600

    Agregar archivos de proyecto.

---
+++ DatosVehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerAutos
+{
+    class DatosVehiculo
+    {
+        string placa;
+        string marca;
+        string modelo;
+        string color;
+        float precioKilometro;
+
+        public string Placa { get => placa; set => placa = value; }
+        public string Marca { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Color { get => color; set => color = value; }
+        public float PrecioKilometro { get => precioKilometro; set => precioKilometro = value; }
+    }
+}
